ae7bdd67fc752434d7c7e9481cb3f70647d3c9ff
GarupanOjisan <nakamura.motohiro.private@gmail.com>
2020-03-20 03:51:34 +0900

    mysql input plugin

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/garupanojisan/go-embulk
 
 go 1.14
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/joho/sqltocsv v0.0.0-20190824231449-5650f27fd5b6
+)
---
+++ plugins/input/mysql/plugin.go
@@ -0,0 +1,54 @@
+package mysql
+
+import (
+	"bytes"
+	"database/sql"
+	"io"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/sqltocsv"
+)
+
+type MySQL struct {
+	dsn   string
+	query string
+	args  []interface{}
+}
+
+// dsn: refer https://github.com/go-sql-driver/mysql#dsn-data-source-name
+func NewMySQLInput(dsn, query string, args ...interface{}) *MySQL {
+	return &MySQL{
+		dsn:   dsn,
+		query: query,
+		args:  args,
+	}
+}
+
+func (m *MySQL) Read(p []byte) (n int, err error) {
+	db, err := m.connect()
+	if err != nil {
+		return 0, err
+	}
+	defer db.Close()
+
+	rows, err := db.Query(m.query, m.args...)
+	if err != nil {
+		return 0, err
+	}
+	defer rows.Close()
+
+	data, err := sqltocsv.WriteString(rows)
+	if err != nil {
+		return 0, err
+	}
+
+	src := bytes.NewBuffer([]byte(data))
+	dst := bytes.NewBuffer(p)
+	writes, err := io.Copy(dst, src)
+	return int(writes), err
+}
+
+func (m *MySQL) connect() (*sql.DB, error) {
+	return sql.Open("mysql", m.dsn)
+}
